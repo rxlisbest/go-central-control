1b58ca14fb59639c1dfa8f0aac018fcd7542da48
Roy <rxlisbest@gmail.com>
2020-01-08 10:24:24 +0800

    Error output

--- controllers/signal.go
+++ controllers/signal.go
@@ -28,17 +28,26 @@ var reponse = map[string]interface{}{
 	"message": "",
 }
 
-func (c *SignalController) Post() {
-	request := c.Ctx.Input.RequestBody
+func (s *SignalController) Error(code int, message string) {
+	s.Ctx.ResponseWriter.WriteHeader(code)
+	reponse["code"] = code
+	reponse["message"] = message
+	s.Data["json"] = reponse // json对象
+	s.ServeJSON()
+}
+
+func (s *SignalController) Post() {
+	request := s.Ctx.Input.RequestBody
 	err := json.Unmarshal(request, &data)
 	fmt.Println(data)
 	if err != nil {
-		fmt.Println("json.Unmarshal is err:", err.Error())
+		s.Error(400, err.Error())
 		return
 	}
 	b, err := beanstalk.Dial("tcp", "127.0.0.1:11300")
 	if err != nil {
-		fmt.Println("json.Unmarshal is err:", err.Error())
+		s.Error(500, err.Error())
+		return
 	}
 	channel, ok := data ["channel"]
 	if ok {
@@ -47,11 +56,12 @@ func (c *SignalController) Post() {
 
 	_, err = b.Put([]byte(request), 2, 0, 120*time.Second)
 	if err != nil {
-		fmt.Println("json.Unmarshal is err:", err.Error())
+		s.Error(500, err.Error())
+		return
 	}
 	//_, _, _ = b.Reserve(5 * time.Second)
-	c.Data["json"] = data // json对象
-	c.ServeJSON()
+	s.Data["json"] = data // json对象
+	s.ServeJSON()
 	return
 
 	//c.Data["Website"] = string(body[:])
