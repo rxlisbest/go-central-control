19abd2c5144d24ad717b7d633a42604b6510d786
Roy <rxlisbest@gmail.com>
2020-01-09 11:24:45 +0800

    client id

--- controllers/signal.go
+++ controllers/signal.go
@@ -2,14 +2,11 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/cache"
 	"github.com/beanstalkd/go-beanstalk"
-	"strconv"
 	"time"
-
-	//"github.com/beanstalkd/go-beanstalk"
-	//"time"
+	//"fmt"
 )
 
 type SignalController struct {
@@ -24,12 +21,12 @@ var data = map[string]interface{}{
 }
 
 var reponse = map[string]interface{}{
-	"code":    0,
+	"code":    "0",
 	"message": "",
 }
 
 func (s *SignalController) Error(code int, message string) {
-	s.Ctx.ResponseWriter.WriteHeader(code)
+	s.Ctx.Output.Status = code
 	reponse["code"] = code
 	reponse["message"] = message
 	s.Data["json"] = reponse // json对象
@@ -39,32 +36,109 @@ func (s *SignalController) Error(code int, message string) {
 func (s *SignalController) Post() {
 	request := s.Ctx.Input.RequestBody
 	err := json.Unmarshal(request, &data)
-	fmt.Println(data)
 	if err != nil {
 		s.Error(400, err.Error())
 		return
 	}
-	b, err := beanstalk.Dial("tcp", "127.0.0.1:11300")
-	if err != nil {
-		s.Error(500, err.Error())
+	channel := data["channel"]
+	if channel != "0" {
+		switch channel.(type) {
+		case string:
+		default:
+			s.Error(400, "The format of the field `channel` is incorrect")
+			return
+		}
+	} else {
+		s.Error(400, "The field `channel` is required")
 		return
 	}
-	channel, ok := data ["channel"]
-	if ok {
-		b.Tube.Name = strconv.FormatInt(int64(channel.(float64)), 10)
+	addr := beego.AppConfig.String("beanstalkdaddr") + ":" + beego.AppConfig.String("beanstalkdport")
+	conn, err := beanstalk.Dial("tcp", addr)
+	if err != nil {
+		s.Error(400, err.Error())
+		return
 	}
 
-	_, err = b.Put([]byte(request), 2, 0, 120*time.Second)
+	tube := &beanstalk.Tube{Conn: conn, Name: channel.(string)}
+	_, err = tube.Put([]byte(request), 1, 0, 120*time.Second)
 	if err != nil {
 		s.Error(500, err.Error())
 		return
 	}
-	//_, _, _ = b.Reserve(5 * time.Second)
 	s.Data["json"] = data // json对象
 	s.ServeJSON()
 	return
+}
 
-	//c.Data["Website"] = string(body[:])
-	//c.Data["Email"] = "astaxie@gmail.com"
-	//c.TplName = "index.tpl"
+func (s *SignalController) Get() {
+	channel := s.GetString("channel")
+	if channel == "" {
+		s.Error(400, "The params `channel` is required")
+		return
+	}
+	init := s.GetString("init")
+	addr := beego.AppConfig.String("beanstalkdaddr") + ":" + beego.AppConfig.String("beanstalkdport")
+	conn, err := beanstalk.Dial("tcp", addr)
+	if err != nil {
+		s.Error(500, err.Error())
+		return
+	}
+
+	bm, err := cache.NewCache("file", `{}`)
+	if err != nil {
+		s.Error(500, err.Error())
+		return
+	}
+
+	clientId := time.Now().Nanosecond()
+	bm.Put("client_id", clientId, 0*time.Second)
+
+	tubeSet := beanstalk.NewTubeSet(conn, channel)
+
+	// clear queue
+	if init == "1" {
+		for {
+			id, _, err := tubeSet.Reserve(0 * time.Second)
+			if err != nil {
+				break
+			}
+			conn.Delete(id)
+		}
+	}
+
+	timeout, err := beego.AppConfig.Int("beanstalkdreservetimeout")
+	if err != nil {
+		s.Error(500, err.Error())
+		return
+	}
+
+	id, body, err := tubeSet.Reserve(time.Duration(int(time.Duration(timeout) * time.Second)))
+	if err != nil {
+		s.Error(408, err.Error())
+		return
+	}
+
+	currentClientId := bm.Get("client_id")
+	if (currentClientId != clientId) {
+		s.Error(409, "Request expired")
+		return
+	}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		s.Error(500, err.Error())
+		return
+	}
+	conn.Delete(id)
+	s.Data["json"] = data // json对象
+	s.ServeJSON()
+	return
+	//for {
+	//	select {
+	//	default:
+	//		id, body, _ := tubeSet.Reserve(5 * time.Second)
+	//		conn.Delete(id)
+	//		fmt.Print(1)
+	//		fmt.Print(string(body[:]))
+	//	}
+	//}
 }
