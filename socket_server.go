654981d00b0cca1bbdbabb07fd9f8dedc873eccd
Roy <rxlisbest@gmail.com>
2020-01-10 13:22:11 +0800

    socket server log

--- socket_server.go
+++ socket_server.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/config"
-	_ "github.com/astaxie/beego/config"
 	"github.com/beanstalkd/go-beanstalk"
+	"github.com/op/go-logging"
 	"net"
+	"os"
 	"time"
 )
 
@@ -16,74 +16,100 @@ var response = map[string]interface{}{
 	"message": "",
 }
 
-func Error(code int, message string) string {
+func Msg(code int, message string) string {
 	response["code"] = code
 	response["message"] = message
 	responseJson, _ := json.Marshal(response)
 	return string(responseJson)
 }
 
+var log = logging.MustGetLogger("example")
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x} %{message}%{color:reset}`,
+)
+
+func initLogging() {
+	backend1 := logging.NewLogBackend(os.Stderr, "", 0)
+	backend2 := logging.NewLogBackend(os.Stderr, "", 0)
+	// For messages written to backend2 we want to add some additional
+	// information to the output, including the used log level and the name of
+	// the function.
+	backend2Formatter := logging.NewBackendFormatter(backend2, format)
+
+	// Only errors and more severe messages should be sent to backend1
+	backend1Leveled := logging.AddModuleLevel(backend1)
+	backend1Leveled.SetLevel(-1, "")
+	// Set the backends to be used.
+	logging.SetBackend(backend1Leveled, backend2Formatter)
+}
+
 func main() {
+	initLogging()
+
 	workerConfig, err := config.NewConfig("json", "./conf/worker.json")
 	if err != nil {
-		fmt.Printf(Error(500, err.Error()))
+		log.Error(err)
 	}
 	workers, err := workerConfig.DIY("workers")
 	if err != nil {
-		fmt.Printf(Error(500, err.Error()))
+		log.Error(err)
 	}
 	for _, v := range workers.([]interface{}) {
-		host := v.(map[string]interface{})["host"]
-		port := v.(map[string]interface{})["port"]
-		protocol := v.(map[string]interface{})["protocol"]
-		channel := v.(map[string]interface{})["channel"]
-		// simple tcp server
-		// 1.listen ip+port
-		listener, err := net.Listen(protocol.(string), host.(string)+":"+port.(string))
-		if err != nil {
-			fmt.Printf(Error(500, err.Error()))
-			return
-		}
-
-		// 2.accept client request
-		// 3.create goroutine for each request
-
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Printf(Error(500, err.Error()))
-		}
-
 		//create goroutine for each connect
-		go process(conn, channel.(string))
+		go process(v.(map[string]interface{}))
 	}
-	time.Sleep(10 * time.Second)
+	time.Sleep(10000 * time.Second)
 }
 
-func process(conn net.Conn, channel string) {
-	defer conn.Close()
-	addr := beego.AppConfig.String("beanstalkdaddr") + ":" + beego.AppConfig.String("beanstalkdport")
-	b, err := beanstalk.Dial("tcp", addr)
-	tubeSet := beanstalk.NewTubeSet(b, channel)
+func process(worker map[string]interface{}) {
+	host := worker["host"]
+	port := worker["port"]
+	protocol := worker["protocol"]
+	channel := worker["channel"]
+	// simple tcp server
+	// 1.listen ip+port
+
+	listener, err := net.Listen(protocol.(string), host.(string)+":"+port.(string))
 	if err != nil {
+		log.Error(err)
 		return
 	}
-	timeout, err := beego.AppConfig.Int("beanstalkdreservetimeout")
+	defer listener.Close()
 
-	fmt.Printf("success")
+	// 2.accept client request
+	// 3.create goroutine for each request
 	for {
-		id, body, err := tubeSet.Reserve(time.Duration(int(time.Duration(timeout) * time.Second)))
+		conn, err := listener.Accept()
 		if err != nil {
+			log.Error(err)
+			break
 		}
 
-		b.Delete(id)
-		//var buf [128]byte
-		//n, err := conn.Read(buf[:])
-		//if err != nil {
-		//	fmt.Printf("read from connect failed, err: %v\n", err)
-		//	break
-		//}
-		str := body
-		//fmt.Printf("receive from client, data: %v\n", str)
-		conn.Write(str)
+		defer conn.Close()
+		addr := beego.AppConfig.String("beanstalkdaddr") + ":" + beego.AppConfig.String("beanstalkdport")
+		b, err := beanstalk.Dial("tcp", addr)
+		tubeSet := beanstalk.NewTubeSet(b, channel.(string))
+		if err != nil {
+			log.Error(err)
+			break
+		}
+		timeout, err := beego.AppConfig.Int("beanstalkdreservetimeout")
+
+		for {
+			id, body, err := tubeSet.Reserve(time.Duration(int(time.Duration(timeout) * time.Second)))
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+
+			b.Delete(id)
+			str := body
+			_, err = conn.Write(str)
+			if err != nil {
+				log.Error(err)
+				conn.Close()
+				break
+			}
+		}
 	}
 }
